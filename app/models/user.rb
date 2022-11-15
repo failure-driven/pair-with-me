9c8ac87b14d2b5680039c2cfeb644d00cdcff019
Michael Milewski <saramic@gmail.com>
2022-11-16 09:07:46 +1100

    Extract github nickname as username

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name # assuming the user model has a name
-      user.username = auth.info.email[/^[^@]+@/].gsub(/[^a-z^A-Z\-_0-9]/, "")
+      user.username = auth.info.nickname if auth.provider == "github"
+      user.username ||= auth.info.email[/^[^@]+@/].gsub(/[^a-z^A-Z\-_0-9]/, "")
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
