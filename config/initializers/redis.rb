55bbe15024cc7f04a1948382510d167ec57898f1
Michael Milewski <saramic@gmail.com>
2023-01-21 14:11:32 +1100

    Added Heroku Data for Redis and removed Upstash
    as upstash was very expensive charging per request and some 100k
    requests are made every 24 hours even when doing nothing but just
    supporting sidekiq

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,4 @@
 # NOTE: not sure if this is necessary? also We seem to be hitting the
 # pay-per/request redis api at an alarming rate, not sure if I need this or
 # simply the setting in sidekiq.rb
-# $redis = Redis.new(url: ENV.fetch("UPSTASH_REDIS_URL", "redis://127.0.0.1:6379"))
+# $redis = Redis.new(url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379"))
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ require "sidekiq"
 require "sidekiq/api"
 
 Rails.configuration.to_prepare do
-  connection_url = ENV.fetch("UPSTASH_REDIS_URL", "redis://127.0.0.1:6379")
+  connection_url = ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379")
 
   Sidekiq.configure_client do |config|
     config.redis = {url: connection_url}
